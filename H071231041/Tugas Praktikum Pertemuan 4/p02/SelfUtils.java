990b2817ff79fba0def1829f110e5efea14aac3f
RexSeizen <seizenB@gmail.com>
2024-04-20 17:08:09 +0800

    Tugas Praktikum 4

---
+++ H071231041/Tugas Praktikum Pertemuan 4/p02/SelfUtils.java
@@ -0,0 +1,8 @@
+public class SelfUtils {
+    public static void displaySelfData() {
+        System.out.println();
+        System.out.println("SELF DATA FROM SELF UTILS");
+        System.out.println("A. TASDIK BIJAKSANA");
+        System.out.println("H071231041");
+    }
+}
