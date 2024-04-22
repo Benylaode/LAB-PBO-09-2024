2b42e4594dc7975378c26635e15d8aa3045f6e89
Zahraasalsabila <143924394+Zahraasalsabila@users.noreply.github.com>
2024-04-22 11:29:15 +0800

    Add files via upload
    tugas 4

---
+++ H071231035/Praktikum-4/Series/SelfUtils.java
@@ -0,0 +1,9 @@
+package Series;
+
+public class SelfUtils {
+    public static void displaySelfData(){
+        System.out.println("Zahra Salsabila");
+        System.out.println("H071231035");
+        System.out.println();
+    }
+}
